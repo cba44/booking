cabf5598ea2167c9d5cdb692a0b9d6860624815f
cba44 <cba_44@yahoo.com>
2023-07-24 13:33:10 +0200

    dockerized frontend

--- booking-frontend/src/http-common.js
+++ booking-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_APP_BASEURL || "http://localhost:3000/",
   headers: {
     "Content-type": "application/json"
   }
